a774e0874e54883e508e737ea51ae6a48773e37b
Preetam Raj <iitianpreetam@gmail.com>
2021-12-06 06:44:27 +0530

    added gitignore

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long int
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define INF 1e18+9
+#define endl '\n'
+using namespace std;
+#define M 1000000007
+#define MAX 1000001
+
+int main(){
+
+	return 0;
+}
